6d7793cb55275078347bca73a22d756ce86e3a12
llonyai <llonyai@gmail.com>
2021-06-25 20:21:28 +0200

    Balu 9 éves

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Balu 9. szülinapja">
+      <p>Balu és Lobonc</p>
+      <StaticImage
+        alt="Balu 9. szülinapja"
+        src="../images/Balu.jpg"
+      />
     </Layout>
   )
 }
